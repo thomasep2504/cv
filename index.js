c8ba4b8e1838fd8b787c3454d9c28d0efbbad03c
thomasep2504 <80606737+thomasep2504@users.noreply.github.com>
2021-03-13 20:36:46 -0500

    Added initial CV website

---
+++ index.js
@@ -0,0 +1,4 @@
+var image=document.querySelector(".abc");
+image.addEventListener("mouseover",function (){image.src = "images/emanuel123.png"});
+image.addEventListener("click",function (){image.src = "images/emanuel123.png"});
+image.addEventListener("mouseout",function (){image.src = "images/emanuel.png"});
